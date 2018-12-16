46360dc340a5a6ab153bcfa7d2621fcab2a28149
jlamberti <jlamberti2015@gmail.com>
2018-12-15 20:41:36 -0700

    final submission

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 var app = express();
 
+
+var session = require('express-session')
+
+app.use(session({
+  secret: 'vimeo-secret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
+app.use(express.json() );       // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: false })); // to support URL-encoded bodies
 
-const getGenres = () => {
-  try {
-    return axios.get('https://api.vimeo.com/ondemand/genres?access_token=675158fe38a61d2601b865875b53d506&fields=name,canonical')
-  } catch (error) {
-    console.error(error)
-  }
-}  
+app.set('port', (process.env.PORT || 5000));
 
-const getGenre = (genre) => {
-  try {
-    return axios.get('https://api.vimeo.com/ondemand/genres/' + genre + '/pages/?access_token=675158fe38a61d2601b865875b53d506&fields=name,description,link,colors,pictures.uri')
-  } catch (error) {
-    console.error(error)
-  }
-}
+// We have html and js in the public directory that need to be accessed
+app.use(express.static(path.join(__dirname, 'public')))
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: false }))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => {
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+app.get('/', verifyLogin, (req, res) => {
     getGenres()
     .then(response => {
       res.render('project2/index', {data: response['data']['data']})
@@ -42,7 +42,8 @@ app
       res.send("fail" + error)
     })
   })
-  .get('/genres/:id', (req, res) => {
+
+app.get('/genres/:id', verifyLogin, (req, res) => {
     getGenre(req.params.id)
     .then(response => {
       const data = [];
@@ -61,18 +62,71 @@ app
     .catch(error => {
       res.send("fail" + error)
     })})
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+app.get('/login', (req, res) => {
+  res.render('project2/login')
+});
 
+// Setup our routes
+app.post('/login', handleLogin);
+app.get('/logout', handleLogout)
+// app.post('/logout', handleLogout);
 
-  // .get('/categories/:id', (req, res) => res.render('project2/categories', {details: req.params.id}))
-  // .get('/week09', (req, res) => {console.log("did it"); res.render('pages/Week09')})
-  // .get('/project2', (req, res) => res.render('project2/index'))
-  // .post('/getRate', (req, res) => res.render('pages/getRate', {name: req.body.name,}))
-  // .get('/getRate', (req, res) => console.log("test"))
-  // .post('/test', (req, res) => console.log("test"))
-  // .post('/getRate', (req, res) => console.log(req.body))
-   // req.body object has your form values
-   // console.log(req.body.clubname);
-   // console.log(req.body.clubtype);
-  // .post('/getRate', (req, res) => res.render('pages/getRate', {details:req.body,}))
+function getGenres() {
+  try {
+    return axios.get('https://api.vimeo.com/ondemand/genres?access_token=675158fe38a61d2601b865875b53d506&fields=name,canonical')
+  } catch (error) {
+    console.error(error)
+  }
+}  
+
+function getGenre(genre) {
+  try {
+    return axios.get('https://api.vimeo.com/ondemand/genres/' + genre + '/pages/?access_token=675158fe38a61d2601b865875b53d506&fields=name,description,link,colors,pictures.uri')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function handleLogin(request, response) {
+  var result = {success: false};
+
+  // We should do better error checking here to make sure the parameters are present
+  if (request.body.username == "admin" && request.body.password == "cs313") {
+    request.session.user = request.body.username;
+    result = {success: true};
+  }
+
+  response.json(result);
+}
+
+function handleLogout(request, response) {
+  var result = {success: false};
+
+  // We should do better error checking here to make sure the parameters are present
+  if (request.session.user) {
+    request.session.destroy();
+    result = {success: true};
+  }
+
+  // response.json(result);
+  response.redirect('/')
+}
+
+function verifyLogin(request, response, next) {
+  if (request.session.user) {
+    // logged in
+    next();
+  } else {
+    // not logged in
+    response.render('project2/login')
+  }
+}
+
+// This middleware function simply logs the current request to the server
+function logRequest(request, response, next) {
+  console.log("Received a request for: " + request.url);
+
+  // don't forget to call next() to allow the next parts of the pipeline to function
+  next();
+}
